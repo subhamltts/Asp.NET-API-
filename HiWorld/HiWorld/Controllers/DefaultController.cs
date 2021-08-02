16304e138c7dcc0c2f843b6c34094b21f3258068
subhamltts <subham.panda@ltts.com>
2021-08-02 20:17:25 +0530

    Morining Activity

---
+++ HiWorld/HiWorld/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using HiWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HiWorld.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        IList<Student> Students = new List<Student>()
+        {
+            new Student()
+                {
+                    StudentId = 1, StudentName = "Mukesh Kumar", Address = "New Delhi", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 2, StudentName = "Banky Chamber", Address = "London", Course = "HR"
+                },
+                new Student()
+                {
+                    StudentId = 3, StudentName = "Rahul Rathor", Address = "Laxmi Nagar", Course = "IT"
+                },
+                new Student()
+                {
+                    StudentId = 4, StudentName = "YaduVeer Singh", Address = "Goa", Course = "Sales"
+                },
+                new Student()
+                {
+                    StudentId = 5, StudentName = "Manish Sharma", Address = "New Delhi", Course = "HR"
+                },
+        };
+        public IList<Student> GetAllStudents()
+        {
+
+            return Students;
+        }
+        public Student GetStudentDetails(int id)
+        {
+
+            var Student = Students.FirstOrDefault(e => e.StudentId == id);
+            if (Student == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return Student;
+        }
+    }
+
+} 
+
+
---
+++ HiWorld/HiWorld/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HiWorld.Models
+{
+   
+       public class Student
+        {
+            public int StudentId
+            {
+                get;
+                set;
+            }
+            public string StudentName
+            {
+                get;
+                set;
+            }
+            public string Address
+            {
+                get;
+                set;
+            }
+            public string Course
+            {
+                get;
+                set;
+            }
+
+        }
+    }
